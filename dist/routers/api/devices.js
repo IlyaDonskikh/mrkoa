b4d6661e4a90f5cffca0354e56b45fbf44a1895a
Ilia Donskikh <donivrecord@gmail.com>
2020-04-18 21:47:02 +0300

    Remove dist files

--- dist/routers/api/devices.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const devices = require("../../controllers/devices");
-const router = new Router();
-// Devices
-router.get('/', devices.index);
-// Export
-exports.default = router.routes();
--- dist/routers/api/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const events = require("../../controllers/events");
-const router = new Router();
-// Devices
-router.get('/events', events.index);
-// Export
-exports.default = router.routes();
