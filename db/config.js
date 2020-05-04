548f194dd57588e62f26c309cbf04a016d95c110
Ilia Donskikh <donivrecord@gmail.com>
2020-05-04 18:49:42 +0300

    Add sequalize client to dev

--- db/config.js
+++ db/config.js
@@ -11,5 +11,10 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
