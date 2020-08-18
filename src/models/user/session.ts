fd873482f30dd08beb6f40257d611e65e45dea32
Ilia Donskikh <donivrecord@gmail.com>
2020-08-18 20:33:36 +0300

    Add sessions migration

---
+++ src/models/user/session.ts
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserSession extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  UserSession.init({
+    token: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'UserSession',
+  });
+  return UserSession;
+};
