54d75763be0df30d46e176aef29ae577adafbce0
Ilia Donskikh <donivrecord@gmail.com>
2020-12-14 21:26:30 +0300

    Cleanup migration

--- test/setup.ts
+++ test/setup.ts
@@ -14,22 +14,26 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 beforeEach('Clean Database', async () => {
-  // As alternative we can use sequelize.sync({ force: true })
-  // It removes umzug and sequelize.drop() logic above, but
-  // data structure will be based on models, not real db structure.
-
   const { models } = db.sequelize;
+  const transaction = await db.sequelize.transaction();
 
   // TODO: Truncate all tables in one request based on table_names
 
-  const promises = Object.keys(models).map(async (modelKey: any) => {
-    await models[modelKey].destroy({
-      truncate: true,
-      cascade: true,
+  try {
+    Object.keys(models).forEach(async (modelKey: any) => {
+      await models[modelKey].destroy(
+        {
+          truncate: true,
+          cascade: true,
+        },
+        { transaction },
+      );
     });
-  });
 
-  await Promise.all(promises);
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+  }
 });
 
 after(async () => {
