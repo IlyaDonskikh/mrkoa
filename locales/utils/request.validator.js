0ee5ba1809723bd1db8394396efbe33cf7634317
Ilia Donskikh <donivrecord@gmail.com>
2021-01-03 16:08:34 +0300

    Add locales

--- locales/utils/request.validator.js
+++ locales/utils/request.validator.js
@@ -2,4 +2,10 @@ module.exports = {
   email: {
     required: 'fill in the filed',
   },
+  password: {
+    required: 'fill in the filed',
+  },
+  passwordConfirmation: {
+    required: 'fill in the filed',
+  },
 };
