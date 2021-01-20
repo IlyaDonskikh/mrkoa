dcf595db57d6e7abd126fb0292a5afc93d16be1c
Ilia Donskikh <donivrecord@gmail.com>
2021-01-20 10:18:50 +0300

    Simplify routes

--- src/routers/api/v1/index.router.ts
+++ src/routers/api/v1/index.router.ts
@@ -1,14 +1,24 @@
 import * as Router from 'koa-router';
-import { panelRoutes } from './panel/index.router';
-import { authRoutes } from './auth/index.router';
+import * as sessions from '../../../controllers/api/v1/auth/sessions.controller';
+import * as users from '../../../controllers/api/v1/panel/users.controller';
+import { authRouterHelper } from '../../helpers/auth.router.helper';
 
 const router = new Router();
 
-// Auth
-router.use('/auth', authRoutes);
+//! Auth
 
-// Panel
-router.use('/panel', panelRoutes);
+// Sessions
+router.post('/auth/sign_in', sessions.create);
+router.use('/auth/sign_out', authRouterHelper);
+router.delete('/auth/sign_out', sessions.destroy);
+
+//! Panel
+
+router.use('/panel', authRouterHelper);
+
+// Users
+router.get('/panel/users', users.index);
+router.post('/panel/users', users.create);
 
 // Export
 export const v1Routes = router.routes();
