ad5e9ab7e30d5bd8ffff717356768f0a162bc89d
Ilia Donskikh <donivrecord@gmail.com>
2021-05-25 23:37:04 +0300

    Update usecase package

--- src/utils/use.case.ts
+++ src/utils/use.case.ts
@@ -1,6 +1,6 @@
 import { MrUseCase } from 'mr-use-case';
 import { ErrorsBuilder } from './errors.builder';
 
-export function UseCase<T, R>() {
+export function UseCase<T extends object | null, R extends object | null>() {
   return MrUseCase<T, R>({ errorsBuilder: ErrorsBuilder });
 }
